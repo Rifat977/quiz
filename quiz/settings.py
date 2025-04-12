9675082b5b747266988b389e086324618840e06b
rifat977 <itscrifat5147@gmail.com>
2025-04-12 23:19:47 +0600

    fix staticurl

--- quiz/settings.py
+++ quiz/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Settings for Media
 MEDIA_URL = '/media/'
