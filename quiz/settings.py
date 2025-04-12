3bbb5d58a1e0fcb7da26bb76696d3aac40294799
rifat977 <itscrifat5147@gmail.com>
2025-04-12 22:51:27 +0600

    fix staticurl

--- quiz/settings.py
+++ quiz/settings.py
@@ -130,9 +130,8 @@ JAZZMIN_SETTINGS = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    '/home/lyradcwo/quiz/staticfiles/',
-]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
